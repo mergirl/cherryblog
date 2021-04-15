eb7f82d32d9820ca86d5d6987a5603163a0af0e7
mergirl <nilsonferreira_@hotmail.com>
2021-04-15 14:52:05 -0300

    Configurando settings para receber api de autenticação do google

--- cherrywebsite/settings.py
+++ cherrywebsite/settings.py
@@ -45,7 +45,20 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
+LOGIN_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
