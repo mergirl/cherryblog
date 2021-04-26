984f9ddfa107ae28d329148a1b3957e123c3eb5b
mergirl <nilsonferreira_@hotmail.com>
2021-04-26 19:49:08 -0300

    arquivos estaticos

--- cherrywebsite/urls.py
+++ cherrywebsite/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('account/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
